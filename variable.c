a0ed1261066b72dd763d1e7fd41debb1ff9229c3
Ian629 <mugambiiann@gmail.com>
2024-09-18 09:36:17 +0300

    Add files via upload

---
+++ variable.c
@@ -0,0 +1,34 @@
+//C variables and data type
+//preprocessor directive
+#include <stdio.h> // scanf() , printf()
+int main () {
+    char  d = 'D';//%s
+    
+    char name[] = "Dufla Akapela"; //%s
+    
+    int age = 0,years ; //0-2 //%d
+    
+     float area =25000 ; //%f
+     
+     double game =  101; //%d
+     
+    double salary =250000; //%lf
+    
+    printf("The character is %c \n" ,d);
+    
+    
+    printf("The string name is %s \n" ,name );
+    
+    
+    printf("The age is %dyears \n" , 21 );
+    
+    
+    printf("The area is %5fhectares \n" , area );
+	
+	printf("The game liked by Akapela is %2d Pes~ \n") ;
+	
+	 printf("The salary is %fKSh \n" ,salary );
+    
+    
+}
+
